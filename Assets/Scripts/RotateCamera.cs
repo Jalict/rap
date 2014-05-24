3b432fc025542770949af35d74583e4be4ea41ae
Frans Peter Larsen <larsen.frans@gmail.com>
2014-05-24 20:24:31 +0200

    Camear

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour {
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (player.position, Vector3.up, Input.GetAxis ("Horizontal"));
+		transform.LookAt (player.position);
+		transform.position = new Vector3 (transform.position.x, player.position.y, transform.position.z);
+	}
+}
